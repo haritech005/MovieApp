602482ff95652450ecdbac69513fb4d57b2247ae
hariharanj-01 <hariharanj@lumel.com>
2025-02-11 21:38:34 +0530

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
